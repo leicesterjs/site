8c079f0782eee3fbb58342a2b3c8c99822898ad3
Dan Jordan <daniel.jordan@mindera.com>
2019-08-28 09:17:46 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+module.exports = eleventyConfig => {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: (err, browserSync) => {
+        const content_404 = fs.readFileSync("_site/404/index.html");
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
+  return {
+    dir: {
+      includes: "_includes",
+      input: "src",
+      layouts: "_layouts"
+    }
+  };
+};
